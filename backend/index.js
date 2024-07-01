28ff6b0c8ae460bc8087ddbed10c1382720d27e6
nawrazaltai <nawraz.altai@chasacademy.se>
2024-07-01 12:51:38 +0200

    code cleanup in backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,5 @@
 import express from "express";
-import jwt from "jsonwebtoken";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import authenticateJWT from "./middleware/authMiddleware.js";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
@@ -13,8 +10,6 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
-const SECRET_KEY = process.env.SECRET_KEY;
-// const MONGO_DB_URI = process.env.MONGO_DB_URI;
 
 connectDB();
 
